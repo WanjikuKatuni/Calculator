dc2d02dd0b56fb64017ced4734729ef23fc8416a
wanjiku <annritawanjiku77@gmail.com>
2022-03-06 22:47:07 +0300

    add js stylesheet for calculator

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//add function
+var add = function(num1,num2) {
+    return num1+num2
+    }
+    var num1 = parseInt (prompt('enter first number'))
+    
+    var num2= parseInt (prompt('enter other number'))
+    
+    alert ('the sum is ' + add(num1,num2))
+    
+    var subtract= function(num1,num2) a
+    return num1-num2
+    }
+    var num1 = prompt('enter fist number')
+    var num2 = prompt ('enter other number')
+    alert ('the answer is ' + subtract(num1,num2))
